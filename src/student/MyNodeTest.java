5f98336112dc7dd230fc5e71401a0cd31d382ce9
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-04-09 15:04:58 +0100

    Add initial test for MyNode constructor

---
+++ src/student/MyNodeTest.java
@@ -0,0 +1,13 @@
+package student;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MyNodeTest {
+
+	@Test
+	public void testsConstructor() {
+		MyNode node = new MyNodeImpl(1);
+	}
+
+}
