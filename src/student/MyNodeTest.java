e6f465dc7ce6f9e9fb01dffb550e096ae4731586
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-04-09 15:15:20 +0100

    Add test for getVisited()

--- src/student/MyNodeTest.java
+++ src/student/MyNodeTest.java
@@ -10,4 +10,11 @@ public class MyNodeTest {
 		MyNode node = new MyNodeImpl(1);
 	}
 
+	@Test
+	public void testsGetVisited() {
+		MyNode node = new MyNodeImpl(1);
+		boolean expected = false;
+		boolean output = node.getVisited();
+		assertEquals(expected, output);
+	}
 }
