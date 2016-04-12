28d38820191468ee93c028e70b0448b7f588dc70
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-04-12 21:20:58 +0100

    Implement addNode() and getNode()

---
+++ src/student/MyCavernImpl.java
@@ -0,0 +1,26 @@
+package student;
+
+import java.util.ArrayList;
+
+public class MyCavernImpl implements MyCavern {
+	private ArrayList<MyNode> nodes = new ArrayList<>();
+	
+	MyCavernImpl() {
+		
+	}
+	
+	public void addNode(long id) {
+		MyNode node = new MyNodeImpl(id);
+		nodes.add(node);
+	}
+	
+	public MyNode getNode(long id) {
+		for(MyNode node : nodes) {
+			if(node.getId() == id) {
+				return node;
+			}
+		}
+		return null;
+	}
+	
+}
