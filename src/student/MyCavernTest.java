9239f1b0b0a7b54d91db7d56a632f016c936ee7b
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-04-12 23:11:22 +0100

    Add testsGetNextKeepsRetracingToUnvisited

--- src/student/MyCavernTest.java
+++ src/student/MyCavernTest.java
@@ -80,4 +80,38 @@ public class MyCavernTest {
 		assertEquals(expected, output);
 	}
 
+	@Test public void testsGetNextKeepsRetracingToUnvisited() {
+		MyCavern cavern = new MyCavernImpl();
+		cavern.addNode(1);
+		cavern.getNode(1).setVisited();
+		cavern.getNode(1).addNeighbour(2);
+		cavern.setLocation(1);
+		cavern.addNode(2);
+		cavern.getNode(2).setVisited();
+		cavern.getNode(2).addNeighbour(1);
+		cavern.getNode(2).addNeighbour(3);
+		cavern.setLocation(2);
+		cavern.addNode(3);
+		cavern.getNode(3).setVisited();
+		cavern.getNode(3).addNeighbour(2);
+		cavern.getNode(3).addNeighbour(4);
+		cavern.getNode(3).addNeighbour(6);
+		cavern.setLocation(3);
+		cavern.addNode(4);
+		cavern.getNode(4).setVisited();
+		cavern.getNode(4).addNeighbour(3);
+		cavern.getNode(4).addNeighbour(5);
+		cavern.setLocation(4);
+		cavern.addNode(5);
+		cavern.getNode(5).setVisited();
+		cavern.getNode(5).addNeighbour(4);
+		cavern.setLocation(5);
+		cavern.addNode(6);
+		cavern.setLocation(cavern.getNext());
+		cavern.setLocation(cavern.getNext());
+		long expected = 6;
+		long output = cavern.getNext();
+		assertEquals(expected, output);
+	}
+	
 }
