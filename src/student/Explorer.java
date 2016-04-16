9e6ff05868e4f06d92f75d201a714005123c7cdb
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-04-16 22:52:23 +0100

    Give a bit more time to escape

--- src/student/Explorer.java
+++ src/student/Explorer.java
@@ -121,7 +121,7 @@ public class Explorer {
     	    	
     	MyNode node = cavern.getNode(state.getCurrentNode().getId());
     	
-       	while((state.getTimeRemaining()/12) > node.getPathLength())  {
+       	while((state.getTimeRemaining()/14) > node.getPathLength())  {
         	// set location
     		cavern.setLocation(node.getId());
      		
